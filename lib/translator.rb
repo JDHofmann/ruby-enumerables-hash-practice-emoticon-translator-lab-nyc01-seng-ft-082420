52bb8d1bbd427c9366994cc88eb5a44445275b43
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 21:05:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(emoticons)
   new_hash = {}
   # evens = (1..10).each_with_object([]) { |i, a| a << i*2 }
   emoticons.each do |key, value|
-  new_hash[key] = "English"
+  new_hash[key] = {}
   binding.pry 
 end 
 end
