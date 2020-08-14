c7483c878e2022bef53a3f36cce529ceaf1a154e
JD Brewer Hofmann <justindhofmann@gmail.com>
2020-08-14 22:52:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -37,7 +37,7 @@ def get_japanese_emoticon(filepath, emo)
       puts "yes"
       # result = emoticons[key][:japanese]
     else 
-      puts "Sorry, that emoticon was not found"
+      return "Sorry, that emoticon was not found"
     end 
   #binding.pry
   end
